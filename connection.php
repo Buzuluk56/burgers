b96c475162414e170c30ff96d6f41ec11fbe0431
Anton Semenov <buzuluk56@yandex.ru>
2020-03-22 18:20:00 +0500

    Готовое задание разбил на функции + отправка на почту функцией Swiftmailer

--- connection.php
+++ connection.php
@@ -1,2 +1,13 @@
 <?php
     $DB = new PDO('mysql:dbname=db_posts;127.0.0.1','root','');
+    require_once './vendor/autoload.php';
+
+//    $UserRoot = ['host'=>'smtp.yandex.ru','port'=>'465',
+//                 'encryption'=>'ssl','Username'=>'Username',
+//                 'password'=>'Mm89068339933'];
+
+    $host = 'smtp.yandex.ru';
+    $port = 465;
+    $encryption = 'ssl';
+    $Username ='Buzuluk56-1@yandex.ru';
+    $password = 'Mm89068339933';
--- mail.php
+++ mail.php
@@ -1,59 +1,71 @@
 <?php
-    require_once 'connection.php';
+require_once 'connection.php';
 
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $street = $_POST['street'];
-    $home = $_POST ['home'];
-    $part = $_POST ['part'];
-    $appt = $_POST ['appt'];
-    $floor = $_POST ['floor'];
-    $comment = $_POST ['comment'];
-    $order = 'DarkBeefBurger за 500 рублей, 1 шт';
-
-
-    function User($name,$order,$DB)
-    {
-        $result = $DB->query("SELECT * FROM burger WHERE Name LIKE '%" . $name . "%'");
-
-        if ($result->rowCount() > 0) {
-            echo "Вы уже зарегистрированы, мы собираем Ваш заказ <b>$name</b>";
-
-        } else {
-            echo "Спасибо,что выбрали нас. Мы собираем Ваш заказ <b>$name</b>";
-        }
-
-        $query = "INSERT INTO orderburger (`name`,`order`)VALUES ('$name','$order')";
-        $DB->query($query);
-    }
-    function Order($name,$DB,$street,$home,$appt)
-    {
-        date_default_timezone_set('Asia/Yekaterinburg');
-        $a = Date('d m Y H.i');
-        $file = 'Заказ '.$name. " ".$a.' .txt';
-
-        $orderID= $DB->query("SELECT MAX(id) FROM orderburger ");
-        $messageID= $DB->query("SELECT COUNT(id) FROM orderburger WHERE name='$name'");
-
-        $message = "Заказ № ". $orderID->fetch()[0]. ".<br> Ваш заказ будет доставлен по адресу: ул. " . $street
-            . " дом № ".$home . " квартира № ". $appt. "Спасибо! Это уже ". $messageID->fetch()[0]. " заказ";
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$street = $_POST['street'];
+$home = $_POST ['home'];
+$part = $_POST ['part'];
+$appt = $_POST ['appt'];
+$floor = $_POST ['floor'];
+$comment = $_POST ['comment'];
+$order = 'DarkBeefBurger за 500 рублей, 1 шт';
 
 
+function User($name,$order,$DB)
+{
+    $result = $DB->query("SELECT * FROM burger WHERE Name LIKE '%" . $name . "%'");
 
-        if ($put=false)
-        {
-            file_put_contents ("order/$file",$message);
-        }
-        else{
-            $put= mkdir('order',FILE_APPEND);
-            file_put_contents ("order/$file",$message);
-        }
+    if ($result->rowCount() > 0) {
+        echo "Вы уже зарегистрированы, мы собираем Ваш заказ <b>$name</b>";
+
+    } else {
+        echo "Спасибо,что выбрали нас. Мы собираем Ваш заказ <b>$name</b>";
     }
 
-    User($name,$order,$DB);
-    Order($name,$DB,$street,$home,$appt);
+    $query = "INSERT INTO orderburger (`name`,`order`)VALUES ('$name','$order')";
+    $DB->query($query);
+}
+function Order($name,$DB,$street,$home,$appt,$host,$port,$encryption,$Username,$password)
+{
+    date_default_timezone_set('Asia/Yekaterinburg');
+    $a = Date('d m Y H.i');
+    $file = 'Заказ '.$name. " ".$a.' .txt';
 
+    $orderID= $DB->query("SELECT MAX(id) FROM orderburger ");
+    $messageID= $DB->query("SELECT COUNT(id) FROM orderburger WHERE name='$name'");
+
+    $subject = "Заказ № ". $orderID->fetch()[0];
+    $messageOrder = " Ваш заказ будет доставлен по адресу: ул. " . $street
+        . " дом № ".$home . " квартира № ". $appt. "Спасибо! Это уже ". $messageID->fetch()[0]. " заказ";
+
+    try{
+        $transport = new Swift_SmtpTransport($host,$port,$encryption);
+        $transport ->setUsername($Username);
+        $transport->setPassword($password);
+
+        $mailer = new Swift_Mailer($transport);
+
+        $message = (new Swift_Message($subject))
+            ->setFrom(['Buzuluk56-1@yandex.ru'=>'Buzuluk56-1@yandex.ru'])
+            ->setTo(['buzuluk56@yandex.ru'])
+            ->setBody($messageOrder);
+
+        $mailer->send($message);
+
+    }catch(Exception $e){
+        var_dump($e->getMessage());
+        echo '<pre>'. print_r($e->getTrace(),1);
+    }
+
+
+
+
+}
+
+User($name,$order,$DB);
+Order($name,$DB,$street,$home,$appt,$host,$port,$encryption,$Username,$password);
 
 
 
